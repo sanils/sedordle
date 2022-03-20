04b36384207c2541bcc55e66f80c148eca5e729e
Simon Jenner <simjenner3@gmail.com>
2022-03-20 00:04:20 +0000

    We reset at UTC, also clean up with NiceLink

--- src/components/TitleBar.jsx
+++ src/components/TitleBar.jsx
@@ -8,6 +8,15 @@ import {
   FaRegMoon, FaSun, FaRegQuestionCircle, FaExternalLinkAlt,
 } from 'react-icons/fa';
 
+function NiceLink({ text, url }) {
+  return (
+    <Link display="inline-flex" alignItems="center" href={url} isExternal>
+      {text}
+      <FaExternalLinkAlt style={{ marginLeft: '0.5em' }} />
+    </Link>
+  );
+}
+
 // eslint-disable-next-line no-unused-vars
 export default function TitleBar({ gameMode, setGameMode, correctGuessCount }) {
   const { colorMode, toggleColorMode } = useColorMode();
@@ -37,40 +46,29 @@ export default function TitleBar({ gameMode, setGameMode, correctGuessCount }) {
           <ModalBody>
             <VStack>
               <Text>You know how to play.</Text>
-              <Text>Sedordle resets at midnight in your timezone.</Text>
+              <Text>
+                Sedordle resets at midnight
+                {' '}
+                <NiceLink text="UTC" url="https://time.is/UTC" />
+              </Text>
               <Text>
                 See the code for this website on
                 {' '}
-                <Link display="inline-flex" alignItems="center" href="https://github.com/psidex/sedordle" isExternal>
-                  GitHub
-                  <FaExternalLinkAlt style={{ marginLeft: '0.5em' }} />
-                </Link>
+                <NiceLink text="GitHub" url="https://github.com/psidex/sedordle" />
               </Text>
               <Text>Check out these other great games:</Text>
               <UnorderedList>
                 <ListItem>
-                  <Link display="inline-flex" alignItems="center" href="https://www.nytimes.com/games/wordle/index.html" isExternal>
-                    Wordle
-                    <FaExternalLinkAlt style={{ marginLeft: '0.5em' }} />
-                  </Link>
+                  <NiceLink text="Wordle" url="https://www.nytimes.com/games/wordle/index.html" />
                 </ListItem>
                 <ListItem>
-                  <Link display="inline-flex" alignItems="center" href="https://zaratustra.itch.io/dordle" isExternal>
-                    Dordle
-                    <FaExternalLinkAlt style={{ marginLeft: '0.5em' }} />
-                  </Link>
+                  <NiceLink text="Dordle" url="https://zaratustra.itch.io/dordle" />
                 </ListItem>
                 <ListItem>
-                  <Link display="inline-flex" alignItems="center" href="https://www.quordle.com/#/" isExternal>
-                    Quordle
-                    <FaExternalLinkAlt style={{ marginLeft: '0.5em' }} />
-                  </Link>
+                  <NiceLink text="Quordle" url="https://www.quordle.com/#/" />
                 </ListItem>
                 <ListItem>
-                  <Link display="inline-flex" alignItems="center" href="https://octordle.com/" isExternal>
-                    Octordle
-                    <FaExternalLinkAlt style={{ marginLeft: '0.5em' }} />
-                  </Link>
+                  <NiceLink text="Octordle" url="https://octordle.com/" />
                 </ListItem>
               </UnorderedList>
             </VStack>
